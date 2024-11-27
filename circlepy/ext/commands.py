371716b7f634dfe81675907abf6719d18abdd4b1
teaforthecat <teaforthecat@users.noreply.github.com>
2024-11-27 19:51:55 +0000

    ✅ Ready to clone and code.

--- circlepy/ext/commands.py
+++ circlepy/ext/commands.py
@@ -1,8 +1,8 @@
 import click
 
-from project_name.ext.auth import create_user
-from project_name.ext.database import db
-from project_name.models import Product
+from circlepy.ext.auth import create_user
+from circlepy.ext.database import db
+from circlepy.models import Product
 
 
 def create_db():
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for circlepy package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("circlepy", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="circlepy",
+    version=read("circlepy", "VERSION"),
+    description="Awesome circlepy created by teaforthecat",
+    url="https://github.com/teaforthecat/circlepy/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="teaforthecat",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["circlepy = circlepy.__main__:main"]
     },
     extras_require={
         "test": read_requirements("requirements-test.txt")
